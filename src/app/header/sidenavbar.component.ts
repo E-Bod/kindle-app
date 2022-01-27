7241ed346f39746e47f561a26a8dc0faad1e9267
E-Bod <ejb00004@mix.wvu.edu>
2022-01-26 19:17:26 -0500

    Navbars1

---
+++ src/app/header/sidenavbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'kA-sidenavbar', 
+    templateUrl: 'sidenavbar.component.html',
+    styleUrls: ['sidenavbar.component.css']
+})
+export class SideNavBarComponent {
+
+}
